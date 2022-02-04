384ac36bff76e57b356beccf45f5d742d175a233
theo <theo.pannethier@cpe.fr>
2022-02-04 17:15:55 +0100

    exeRCICE4

---
+++ calculator/ __init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  4 16:23:27 2022
+
+@author: theo.pannethier
+"""
+
---
+++ test/test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  4 16:25:24 2022
+
+export PYTHONPATH=$PYTHONPATH:"."
+env|grep PYTHONPATH
+
+python3 -m pylint Programme.py
+
+@author: theo.pannethier
+"""
+
+from calculator.SimpleCalculator import SimpleCalculator
+
+
+
+OBJET = SimpleCalculator()
+print (OBJET.sum2(10, 2))
+print(OBJET.substract(10, 2))
+print(OBJET.divide(10, 2))
+print(OBJET.mul(10, 2))
